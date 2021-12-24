8af66079c556972831dc2a34fb64c658fa507d7f
Ddvrg <bigbosszaa086@gmail.com>
2021-12-24 10:53:48 +0700

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>เว็ปหัดทำ</title>
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun&display=swap" rel="stylesheet">
+    <link href="css/customer.css" rel="stylesheet">
+</head>
