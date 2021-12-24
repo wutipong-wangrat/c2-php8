fa1ccd2ebcbe5ccb483b7a7ba665667d480aa488
Ddvrg <bigbosszaa086@gmail.com>
2021-12-24 10:58:54 +0700

    initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,55 @@
+<body>
+
+    <head>
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #6f42c1;">
+            <div class="container">
+                <a class="navbar-brand" href="#">สวัสดีครับ</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">หน้าหลัก</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                เกี่ยวกับเรา
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">ประวัติส่วนตัว</a></li>
+                                <li><a class="dropdown-item" href="#">เกี่ยวกับเรา</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">หน้าที่รับผิดชอบ</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ทีมงานของเรา</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ภาพความทรงจำ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ติดต่อเรา</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                ลิ้งค์เว็ปไซต์
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">วท.เทคนิคตราด</a></li>
+                                <li><a class="dropdown-item" href="#">อาชีวศึกษา</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">กระทรวงศึกษาธิการ</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </head>
---
+++ includes/scripts.php
@@ -0,0 +1,2 @@
+<!-- Optional Javascript choose one of the two! -->
+<script src="js/bootstrap.bundle.min.js"></script>
